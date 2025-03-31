4888e5cc5c778fea0bd4a8e05ebc9b3f3510d9cf
krikchaip <krikchai.p@gmail.com>
2025-04-01 00:49:03 +0700

    refactor: convert strings into a task log

--- internal/model/log_entry.go
+++ internal/model/log_entry.go
@@ -1,8 +1,21 @@
 package model
 
+import "fmt"
+
 type LogEntry string
 
 const (
 	EntryStart LogEntry = "START"
 	EntryEnd   LogEntry = "END"
 )
+
+func ToLogEntry(s string) (LogEntry, error) {
+	switch LogEntry(s) {
+	case EntryStart:
+		return EntryStart, nil
+	case EntryEnd:
+		return EntryEnd, nil
+	default:
+		return "", fmt.Errorf("Unknown emtry %q", s)
+	}
+}
--- internal/model/task_log.go
+++ internal/model/task_log.go
@@ -1,6 +1,9 @@
 package model
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 const (
 	TIMESTAMP_FORMAT = "15:04:05"
@@ -11,4 +14,33 @@ type TaskLog struct {
 	Description string
 	Entry       LogEntry
 	PID         int
+}
+
+func NewTaskLogFromStrings(
+	timestamp,
+	description,
+	entry,
+	pid string,
+) (tl TaskLog, err error) {
+	ts, err := time.Parse(TIMESTAMP_FORMAT, timestamp)
+	if err != nil {
+		return tl, err
+	}
+
+	id, err := strconv.Atoi(pid)
+	if err != nil {
+		return tl, err
+	}
+
+	ent, err := ToLogEntry(entry)
+	if err != nil {
+		return tl, err
+	}
+
+	tl.Timestamp = ts
+	tl.Description = description
+	tl.Entry = ent
+	tl.PID = id
+
+	return
 }
