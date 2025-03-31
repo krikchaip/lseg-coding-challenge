5c316d5fc3ee1e29c05e80d8a73896dbdf084be7
krikchaip <krikchai.p@gmail.com>
2025-03-31 21:37:10 +0700

    feat: parse csv file (data/logs.log)

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	file, err := os.Open("data/logs.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := csv.NewReader(file)
+
+	for {
+		record, err := r.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(record)
+	}
 }
