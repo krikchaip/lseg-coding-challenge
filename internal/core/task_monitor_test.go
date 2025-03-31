c707a31328a537da87c7fabf2f431057541b5ac4
krikchaip <krikchai.p@gmail.com>
2025-04-01 04:25:20 +0700

    test: add case if time took > 5 mins, but <= 10mins

--- internal/core/task_monitor_test.go
+++ internal/core/task_monitor_test.go
@@ -30,6 +30,32 @@ func TestTaskMonitor(t *testing.T) {
 			t.Error("expect reporter not to be called.")
 		}
 	})
+
+	t.Run("Warn if a job took > 5mins, but <= 10mins", func(t *testing.T) {
+		var reporter TestReporter
+
+		timestamp := toTimestamp("00:00:00")
+		monitor := core.NewTaskMonitor(&reporter)
+
+		monitor.AppendLog(model.TaskLog{
+			PID:       1,
+			Entry:     model.EntryStart,
+			Timestamp: timestamp,
+		})
+		monitor.AppendLog(model.TaskLog{
+			PID:       1,
+			Entry:     model.EntryEnd,
+			Timestamp: timestamp.Add(10 * time.Minute),
+		})
+
+		if !reporter.WarnCalled {
+			t.Error("expect reporter.Warn() to be called.")
+		}
+
+		if reporter.ErrorCalled {
+			t.Error("expect reporter.Error() not to be called.")
+		}
+	})
 }
 
 func toTimestamp(t string) time.Time {
