2e68e79839bf5b4ff890258f943738dde8dcfd2c
krikchaip <krikchai.p@gmail.com>
2025-03-31 23:10:44 +0700

    feat: task monitor class
    - monitors task records
    - pop task when encountering END record

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -20,7 +20,77 @@ type Task struct {
 	EndedAt     time.Time
 }
 
+type TaskMonitor struct {
+	pendingTasks map[string]*Task
+}
+
+func (this *TaskMonitor) Append(t Task) {
+	pt, ok := this.pendingTasks[t.Description]
+
+	if !ok {
+		this.addNewTask(t)
+		return
+	}
+
+	if !t.StartedAt.IsZero() {
+		log.Printf("START record for task %q already existed.\n", t.Description)
+		return
+	}
+
+	pt.EndedAt = t.EndedAt
+
+	this.popTask(pt)
+}
+
+func (this *TaskMonitor) addNewTask(t Task) {
+	if t.StartedAt.IsZero() {
+		log.Printf("START record not found for task %q.\n", t.Description)
+		return
+	}
+
+	if this.pendingTasks == nil {
+		this.pendingTasks = map[string]*Task{t.Description: &t}
+		return
+	}
+
+	this.pendingTasks[t.Description] = &t
+}
+
+func (this *TaskMonitor) popTask(t *Task) {
+	if t.StartedAt.IsZero() {
+		log.Printf("START record is missing for task %q.\n", t.Description)
+		return
+	}
+
+	if t.EndedAt.IsZero() {
+		log.Printf("END record is missing for task %q.\n", t.Description)
+		return
+	}
+
+	minsTaken := t.EndedAt.Sub(t.StartedAt).Minutes()
+
+	if 5 < minsTaken && minsTaken <= 10 {
+		log.Printf(
+			"[WARNING] task %q, pid %d took %.0fmins to complete.",
+			t.Description,
+			t.Pid,
+			minsTaken,
+		)
+	} else if minsTaken > 10 {
+		log.Printf(
+			"[ERROR] task %q, pid %d took %.0fmins to complete.",
+			t.Description,
+			t.Pid,
+			minsTaken,
+		)
+	}
+
+	delete(this.pendingTasks, t.Description)
+}
+
 func main() {
+	var tm TaskMonitor
+
 	file, err := os.Open("data/logs.log")
 	if err != nil {
 		log.Fatal(err)
@@ -76,6 +146,6 @@ func main() {
 			continue
 		}
 
-		fmt.Println(t)
+		tm.Append(t)
 	}
 }
